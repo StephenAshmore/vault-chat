22d6d7fe8a9a46c72e2fb7f8169bd2ea3efd302f
Stephen Ashmore <StephenCAshmore@gmail.com>
2017-03-11 22:01:08 -0600

    Refactored version of the sockets.io tutorial.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Vault Chat
+Vault Chat is a node.js and socket.io chat service, built for my own practice.
---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+var io = require('../io');
+
+io.on('connection', function(socket){
+  console.log('user connected');
+
+  socket.on('chat message', function(msg){
+    console.log('chat message ' + msg);
+    io.emit('chat message', msg);
+  });
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Vault Chat' });
+});
+
+module.exports = router;
