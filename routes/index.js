d68100afc1ce18a100199da868cb37f943632dc1
Stephen Ashmore <StephenCAshmore@gmail.com>
2017-03-12 14:17:17 -0500

    Incorporated mongodb to handle the users. Now uses passport for authentication.

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var passport = require('passport');
+var Account = require('../models/account');
 var router = express.Router();
 var io = require('../io');
 
@@ -17,14 +19,42 @@ io.on('connection', function(socket){
   });
 });
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  if (req.isAuthenticated()) {
-    res.render('index', { title: 'Vault Chat' });
-  }
-  else {
-    res.render('user', {title: 'Vault Chat Login'});
-  }
+router.get('/', function (req, res) {
+    res.render('index', { user : req.user });
+});
+
+router.get('/register', function(req, res) {
+    res.render('register', { });
+});
+
+router.post('/register', function(req, res, next) {
+    Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
+        if (err) {
+          return res.render('register', { error : err.message });
+        }
+
+        passport.authenticate('local')(req, res, function () {
+            req.session.save(function (err) {
+                if (err) {
+                    return next(err);
+                }
+                res.redirect('/');
+            });
+        });
+    });
+});
+
+router.get('/login', function(req, res) {
+    res.render('login', { user : req.user });
+});
+
+router.post('/login', passport.authenticate('local'), function(req, res) {
+    res.redirect('/');
+});
+
+router.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
 });
 
 module.exports = router;
